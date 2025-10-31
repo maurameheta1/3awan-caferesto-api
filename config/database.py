4f68a6f7375820d503272cbf6934d69f1fae10ef
maurameheta1 <mauramaheta05@gmail.com>
2025-10-31 20:51:44 +0700

    ganti database url

--- config/database.py
+++ config/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # 🔗 URL Database dari Railway
-DATABASE_URL = "postgresql://postgres:lDydWOXAFJiGytjhZprEomwzWwhhKUVD@centerbeam.proxy.rlwy.net:17783/railway"
+DATABASE_URL = "postgresql://postgres:iqTFSbEWnJCvydSLiixmeVQfwErRQfhb@shuttle.proxy.rlwy.net:57568/railway"
 
 # Engine koneksi
 engine = create_engine(DATABASE_URL, echo=False)
